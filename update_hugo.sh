bb6813a649675b977ca0f787eb6dba50bbeabd72
D4rkLynk <74207682+d4rklynk@users.noreply.github.com>
2023-05-28 18:41:31 +0200

    Fix script

--- update_hugo.sh
+++ update_hugo.sh
@@ -1,26 +1,15 @@
-#!/bin/bash
-
 update_hugo_version() {
   file_path=$1
   new_version=$2
 
-  file_content=$(<"$file_path")
-  pattern="HUGO_VERSION = \"[0-9]+\.[0-9]+\.[0-9]+\""
+  pattern='HUGO_VERSION = "[0-9]+\.[0-9]+\.[0-9]+"'
+  replacement="HUGO_VERSION = \"$new_version\""
 
-  if [[ $file_content == *"$new_version"* ]]; then
-    echo "Hugo is already up to date (version $new_version)"
+  if grep -qE "$pattern" "$file_path"; then
+    sed -E "s/$pattern/$replacement/" "$file_path" > "$file_path.tmp" && mv "$file_path.tmp" "$file_path"
+    echo "Hugo version updated to $new_version successfully!"
   else
-    if [[ $file_content =~ $pattern ]]; then
-      updated_content=$(echo "$file_content" | sed "s@$pattern@HUGO_VERSION = \"$new_version\"@")
-      echo "$updated_content" | tee "$file_path"
-      echo "Hugo version updated to $new_version successfully!"
-    else
-      echo "The target line was not found in the file."
-    fi
+    echo "The target line was not found in the file."
   fi
 }
 
-version=$(curl -sSL "https://github.com/gohugoio/hugo/releases/latest" | grep -oP '(?<=/v)\d+\.\d+\.\d+')
-file_path="netlify.toml"
-
-update_hugo_version "$file_path" "$version"
