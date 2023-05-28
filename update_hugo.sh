8731b7de662dc70c4f2327374da79dd4727ccb89
D4rkLynk <74207682+d4rklynk@users.noreply.github.com>
2023-05-28 18:24:09 +0200

    Make it bash

---
+++ update_hugo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+update_hugo_version() {
+  file_path=$1
+  new_version=$2
+
+  file_content=$(<"$file_path")
+  pattern="HUGO_VERSION = \"[0-9]+\.[0-9]+\.[0-9]+\""
+
+  if [[ $file_content == *"$new_version"* ]]; then
+    echo "Hugo is already up to date (version $new_version)"
+  else
+    if [[ $file_content =~ $pattern ]]; then
+      updated_content=$(echo "$file_content" | sed "s/$pattern/HUGO_VERSION = \"$new_version\"/")
+      echo "$updated_content" > "$file_path"
+      echo "Hugo version updated to $new_version successfully!"
+    else
+      echo "The target line was not found in the file."
+    fi
+  fi
+}
+
+version=$(curl -sSL "https://github.com/gohugoio/hugo/releases/latest" | grep -oP '(?<=/v)\d+\.\d+\.\d+')
+file_path="netlify.toml"
+
+update_hugo_version "$file_path" "$version"
+
