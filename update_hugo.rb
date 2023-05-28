3566ccf7199ca034b01753e8f57b339b1440ad56
D4rkLynk <74207682+d4rklynk@users.noreply.github.com>
2023-05-28 17:59:18 +0200

    Add hugo-version workflow

---
+++ update_hugo.rb
@@ -0,0 +1,26 @@
+require 'rest-client'
+
+def update_hugo_version(file_path, new_version)
+  file_content = File.read(file_path)
+
+  pattern = /HUGO_VERSION = "\d+.\d+.\d+"/
+
+  if file_content.include?(new_version)
+      puts "Hugo already up to date (version #{new_version})"
+  else
+    if file_content.match?(pattern)
+      updated_content = file_content.gsub(pattern, "HUGO_VERSION = "#{new_version}"")
+      File.write(file_path, updated_content)
+      puts "Hugo version updated to #{new_version} successfully!"
+    else
+      puts "The target line was not found in the file."
+    end
+  end
+end
+
+res = RestClient.get "https://github.com/gohugoio/hugo/releases/latest"
+version = res.net_http_res.uri.to_s.split("/").last[1..-1]
+
+file_path = 'netlify.toml'
+
+update_hugo_version(file_path, version)
