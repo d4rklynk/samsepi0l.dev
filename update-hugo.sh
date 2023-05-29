6e8d192bc1e4b913bb5f9b599ef2bea867946236
D4rkLynk <74207682+d4rklynk@users.noreply.github.com>
2023-05-29 11:01:49 +0200

    Fix workflow and add script

---
+++ update-hugo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get the latest Hugo version from the GitHub releases API
+latest_version=$(curl --silent "https://api.github.com/repos/gohugoio/hugo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+# Get the current Hugo version from netlify.toml
+current_version=$(grep -Po '(?<=HUGO_VERSION = ")[^"]*' netlify.toml)
+
+# Compare the latest version with the current version
+if [[ "$latest_version" == "$current_version" ]]; then
+  echo "Hugo version $latest_version is already up to date."
+  exit 0
+fi
+
+# Update the HUGO_VERSION variable in netlify.toml
+sed -i "s/^HUGO_VERSION = \".*\"/HUGO_VERSION = \"$latest_version\"/" netlify.toml
+
+# Commit the changes
+git add netlify.toml
+git commit -m "Update Hugo version to $latest_version" --quiet
+git push origin HEAD:main
