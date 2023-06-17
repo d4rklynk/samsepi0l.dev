2dbc02f686c493091e76a26b388413be8e948537
D4rkLynk <74207682+d4rklynk@users.noreply.github.com>
2023-06-17 16:07:23 +0200

    Add minify flag

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -6,7 +6,7 @@ tar xvf ./*.tar.gz
 chmod u+x ./hugo
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
-  ./hugo --gc && rm -rf cache
+  ./hugo --gc --minify && rm -rf cache
 else
-  ./hugo --gc -b "$CF_PAGES_URL" && rm -rf cache
+  ./hugo --gc --minify -b "$CF_PAGES_URL" && rm -rf cache
 fi
